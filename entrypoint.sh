f42d1d431688a8a4b3717fde7d0fa15b96258144
rssdaniezquerra <67096919+rssdaniezquerra@users.noreply.github.com>
2021-02-01 13:56:37 +0100

    Bugfix/rss needs (#2)
    Changed variables to point to default github environment variables.
    
    git push is no longer used. Instead, git fecth is used to retrieve repository information. 
    
    Git push is not even tried if no changes where made. 
    
    Changes on return values to match github actions requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-# Exit on any error and show execution of all commands for debugging if something goes wrong
+commit_and_push () {
+	git config --local user.email "beautify-action@master"
+	git config --local user.name "beautify-action"
+
+	git checkout ${GITHUB_HEAD_REF}
+	git commit -am "Beautify action based on coding style"
+
+	remote_repo="https://x-access-token:${INPUT_REPOTOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+	git remote set-url origin "$remote_repo"
+	git push origin ${GITHUB_HEAD_REF}
+}
+
+
+# Exit on any error
 set -e
 
 cd "$GITHUB_WORKSPACE"
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-DEFAULT_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+DEFAULT_BRANCH="master"
+
+if [[ -n $INPUT_DEFAULTBRANCH ]]; then
+    DEFAULT_BRANCH=$INPUT_DEFAULTBRANCH
+fi
 
 RED="\u001b[31m"
 GREEN="\u001b[32m"
@@ -27,7 +44,10 @@ else
     CONFIG=" -c /default.cfg"
 fi
 
+git fetch origin
+
 EXIT_VAL=0
+NO_CHANGES=0
 
 while read -r FILENAME; do
     TMPFILE="${FILENAME}.tmp"
@@ -44,11 +64,19 @@ while read -r FILENAME; do
 
     if [[ $RETURN_VAL -gt 0 ]]; then
         echo -e "${RED}${OUT} failed style checks.${RESET}"
-        uncrustify${CONFIG} -f ${FILENAME} -o ${TMPFILE} && colordiff -u ${FILENAME} ${TMPFILE}
+        #uncrustify${CONFIG} -f ${FILENAME} -o ${TMPFILE} && colordiff -u ${FILENAME} ${TMPFILE}
+	OUT=$(uncrustify${CONFIG} -f ${FILENAME} -o ${TMPFILE})
+	RETURN_VAL=$?
+	mv ${TMPFILE} ${FILENAME}
         EXIT_VAL=$RETURN_VAL
+	NO_CHANGES=$((NO_CHANGES + 1))
     else
         echo -e "${GREEN}${OUT} passed style checks.${RESET}"
     fi
-done < <(git diff --name-status --diff-filter=AM origin/${DEFAULT_BRANCH}...${BRANCH_NAME} -- '*.cpp' '*.h' '*.hpp' '*.cxx' | awk '{ print $2 }' )
+done < <(git diff --name-status --diff-filter=AM origin/${GITHUB_BASE_REF}...origin/${GITHUB_HEAD_REF} -- '*.cpp' '*.h' '*.hpp' '*.cxx' | awk '{ print $2 }' )
+
+if [[ $NO_CHANGES -gt 0 ]]; then
+	commit_and_push
+fi
 
 exit $EXIT_VAL
